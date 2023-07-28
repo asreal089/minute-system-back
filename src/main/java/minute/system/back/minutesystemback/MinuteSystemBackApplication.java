94204b4728a4becc32dac3421d3d76a7f2111f19
asreal089 <lexx_89@hotmail.com>
2023-07-28 07:26:18 -0300

    Initial project commit

---
+++ src/main/java/minute/system/back/minutesystemback/MinuteSystemBackApplication.java
@@ -0,0 +1,13 @@
+package minute.system.back.minutesystemback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinuteSystemBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinuteSystemBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/minute/system/back/minutesystemback/MinuteSystemBackApplicationTests.java
@@ -0,0 +1,13 @@
+package minute.system.back.minutesystemback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinuteSystemBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
