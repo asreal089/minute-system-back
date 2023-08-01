39078cb6e8c8f9b410bde6832fdb3289d81ce830
asreal089 <lexx_89@hotmail.com>
2023-08-01 04:13:49 -0300

    Adding documantation for swagger

--- src/main/java/minute/system/back/controller/MinuteController.java
+++ src/main/java/minute/system/back/controller/MinuteController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import minute.system.back.model.dto.ApiResponseDTO;
 import minute.system.back.model.dto.minute.MinuteRequestDTO;
@@ -16,7 +20,7 @@ import minute.system.back.model.dto.minute.MinuteResponseDTO;
 import minute.system.back.model.dto.minute.ResultResponseDTO;
 import minute.system.back.service.impl.MinuteServiceImpl;
 
-
+@Tag(name = "Minute", description = "api to create and get minute, and get minute result")
 @RestController
 @RequestMapping("/v1/minute")
 public class MinuteController {
@@ -24,6 +28,11 @@ public class MinuteController {
     @Autowired
     private MinuteServiceImpl minuteService;
     
+    @Operation(summary = "Create a new Minute to be voted", description = "Returns the created Minute")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully created"), 
+        @ApiResponse(responseCode = "400", description = "Invalid request")
+    })
     @PostMapping("/")
     public ResponseEntity<ApiResponseDTO<MinuteResponseDTO>> createMinute(@RequestBody @Valid MinuteRequestDTO minuteRequestDTO) {
 
@@ -32,6 +41,11 @@ public class MinuteController {
         return ResponseEntity.ok(new ApiResponseDTO<MinuteResponseDTO>(response, null));
     }
 
+    @Operation(summary = "Get minute by id", description = "Returns the Minute")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully retrieved"), 
+        @ApiResponse(responseCode = "404", description = "Not found")
+    })
     @GetMapping("/{id}")
     public ResponseEntity<ApiResponseDTO<MinuteResponseDTO>> getMinute(@PathVariable("id") Long id) {
 
@@ -40,6 +54,11 @@ public class MinuteController {
         return ResponseEntity.ok(new ApiResponseDTO<MinuteResponseDTO>(response, null));
     }
 
+    @Operation(summary = "Get results of a minute", description = "Returns the Minute results")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully retrieved"), 
+        @ApiResponse(responseCode = "400", description = "Invalid request")
+    })
     @GetMapping("/{id}/result")
     public ResponseEntity<ApiResponseDTO<ResultResponseDTO>> getMinuteResults(@PathVariable("id") Long id) {
 
--- src/main/java/minute/system/back/controller/VoteController.java
+++ src/main/java/minute/system/back/controller/VoteController.java
@@ -5,6 +5,9 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import minute.system.back.model.dto.ApiResponseDTO;
 import minute.system.back.model.dto.vote.VoteRequestDTO;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+@Tag(name = "Vote", description = "api to vote in a minute")
 @RestController
 @RequestMapping("/v1/minute")
 public class VoteController {
@@ -22,6 +26,10 @@ public class VoteController {
     @Autowired
     private VoteServiceImpl voteServiceImpl;
 
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "Successfully created"), 
+        @ApiResponse(responseCode = "400", description = "Invalid request")
+    })
     @PostMapping("/{idMinute}/vote")
     public ResponseEntity<ApiResponseDTO<VoteResponseDTO>> postMethodName(@PathVariable("idMinute") Long idMinute, @RequestBody @Valid VoteRequestDTO request) {
         VoteResponseDTO response = voteServiceImpl.vote(request, idMinute);
